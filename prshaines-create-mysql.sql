a0491025e7efa92268e279c9e536bc051c1d37c6
JHaines52 <jessica.e.w.haines@gmail.com>
2024-04-08 13:40:14 -0400

    Sql docs

---
+++ prshaines-create-mysql.sql
@@ -0,0 +1,72 @@
+DROP DATABASE IF EXISTS prsjhaines;
+
+CREATE DATABASE prsjhaines;
+
+USE prsjhaines;
+
+-- main differences from sql server:
+-- no GO
+-- no USE master
+-- AUTO_INCREMENT vs IDENTITY(1,1)
+-- No need for square braces
+-- Need semicolons
+
+CREATE TABLE User (
+	Id int PRIMARY KEY AUTO_INCREMENT,
+	Username varchar(20) NOT NULL UNIQUE,
+	Password varchar(10) NOT NULL,
+	Firstname varchar(20) NOT NULL,
+	Lastname varchar(20) NOT NULL,
+	Phone varchar(12),
+	Email varchar(75),
+	Reviewer bit,
+	Admin bit 
+);
+
+CREATE TABLE Vendor (
+	Id int PRIMARY KEY AUTO_INCREMENT,
+	Code varchar(10) NOT NULL UNIQUE,
+	Name varchar(255) NOT NULL,
+	Address varchar(255) NOT NULL,
+	City varchar(255) NOT NULL,
+	Region varchar(255) NOT NULL,
+	Zip varchar(5) NOT NULL,
+	Phone varchar(12),
+	Email varchar(100)
+);
+
+CREATE TABLE Product (
+	Id int PRIMARY KEY AUTO_INCREMENT,
+	VendorId int NOT NULL,
+	PartNumber varchar(50) NOT NULL,
+	Name varchar(150) NOT NULL,
+	Price decimal(10,2) NOT NULL,
+	Unit varchar(255) NOT NULL,
+	PhotoPath varchar(255),
+	CONSTRAINT UQ_Vendor_PartNum UNIQUE (VendorId, PartNumber),
+	FOREIGN KEY (VendorId) REFERENCES Vendor(Id)
+);
+
+CREATE Table Request (
+	Id int PRIMARY KEY AUTO_INCREMENT,
+	UserId int NOT NULL,
+	Description varchar(100) NOT NULL,
+	Justification varchar(255) NOT NULL,
+	DateNeeded date NOT NULL,
+	DeliveryMode varchar(25) NOT NULL DEFAULT 'Pickup',
+	Status varchar(20) NOT NULL DEFAULT 'NEW',
+	Total decimal(10,2) NOT NULL DEFAULT 0.0,
+	SubmittedDate datetime NOT NULL default current_timestamp,
+	ReasonForRejection varchar(100),
+	FOREIGN KEY (UserId) REFERENCES User(Id)
+);
+
+CREATE Table LineItem (
+	Id int PRIMARY KEY AUTO_INCREMENT,
+	RequestId int,
+	ProductId int,
+	Quantity int DEFAULT 1,
+	FOREIGN KEY (RequestId) REFERENCES Request(Id),
+	FOREIGN KEY (ProductId) REFERENCES Product(Id),
+	CONSTRAINT UQ_Req_Pdt UNIQUE (RequestId, ProductId)
+);
---
+++ prshaines-insert-mysql.sql
@@ -0,0 +1,89 @@
+USE prsjhaines;
+
+INSERT INTO User (Username, Password, Firstname, LastName, Phone, Email, Reviewer, Admin)
+VALUES ('YurlunggurSerpent', 'Scale123', 'Prismara', 'Yurlunggur', '111-999-8888', 'RainbowSerpent@dreamtime.au', FALSE, FALSE),
+('EvaEmpimeliad', 'trees45', 'Eldora', 'Empimeliad', '202-555-1234', 'woodnymph@greekmythology.gr', FALSE, FALSE),
+('RadagastBrown', 'Wizard3', 'Radagast', 'TheBrown', '000-234-5678', 'wizard@middleearth.net', FALSE, FALSE),
+('SmaugTheGolden', 'Dragon8', 'Smaug', 'TheGolden', '999-999-0000', 'firedragon@middleearth.net', FALSE, FALSE),
+('TitanusGojira', 'Monster2', 'Godzilla', 'Daikaijus', '555-555-0505', 'titanus@hollowearth.mon', FALSE, FALSE),
+('WorldSerpent', 'Monster4', 'Jörmungandr', 'Midgard Serpent', '233-312-6789', 'jormungandr@midgard.sea', FALSE, FALSE),
+('LinkHero', 'Boy123', 'Link', 'Link', '111-777-0123', 'link@thelegendofzelda.com', FALSE, FALSE),
+('DarkLord', 'Horcrux13', 'Tom', 'Riddle', '000-666-0000', 'voldemort@harrypotter.com', FALSE, TRUE),
+('mando', 'mandalor', 'Din', 'Djarin', '123-456-7890', 'mando@starwars.com', false, false),
+('powerfulMaiar', 'Wizard1', 'Annatar', 'Sauron', '123-456-7890', 'annatar@middleearth.net', false, true),
+('Anubis', 'EgyptGod1', 'Anubis', 'UnderworldGuide', '000-000-0000', 'anubis@acientegypt.eg', false, true),
+('Sasquatch', 'HidAndSeek', 'Sasquatch', 'Bigfoot', '675-565-5835', 'sasquatch@mythicalcreatures.com', false, false),
+('KraotsStrength', 'GreekGod1', 'Kraots', 'son of Pallas', '123-456-7890', 'kraots@greekmythology.gr', false, true);
+
+
+
+-- vendor data
+INSERT INTO Vendor (Code, Name, Address, City, Region, Zip, Phone, Email)
+VALUES
+('RF001', 'Rohirrim Farmer', 'The Great Plains', 'Edoras', 'Rohan', '32045', '123-456-7890', 'farmer@rohan.me'),
+('BYB001', 'Boneyard Beer', '1234 Brew St', 'Bend', 'Oregon', '97703', '541-555-1234', 'info@boneyardbeer.com'),
+('LH2001', 'Life H20', '123 Water St', 'Darwin', 'Australia', '08200', '008-124-5678', 'info@lifeh20.com.au'),
+('PA100', 'Peristeri Attica', '123 Olive Lane', 'Athens', 'Greece', '10431', '030-210-1234', 'contact@peristeriattica.gr'),
+('BB001', 'Borgin and Burkes', '13B Knockturn Alley', 'London', 'England', 'SW1A2', '020-794-0032', 'info@borginandburkes.co.uk'),
+('MT001', 'Metalworking of Thror', 'Forge Lane 1', 'Erebor', 'Lonely Mountain', '00010', '123-456-7890', 'thror@dwarvinforager.com'),
+('UDHE001', 'Radiant Resources', 'Subterranean Outpost 5', 'Inner City', 'Hollow Earth', '00000', '000-000-0000', 'contact@hollowearthuranium.com'),
+('OOH001', 'Odin"s Ocean Harvest', 'Valhalla Lane 9', 'Asgard', 'Midgard', '99900', '154-743-2456', 'contact@odinsocean.me'),
+('ARM001', 'The Armorer', 'Forge Location 1', 'Nevarro', 'Mandalore Sector', '46785', '000-111-2222', 'armorer@mandalore.com'),
+('NOL001', 'Noldorin Elven-smiths', 'Forge Street', 'Eregion', 'Middle-Earth','54321', '000-123-4567', 'Celebrimbor@elvensmiths.com'),
+ ('MAAT001', 'Ma''at Ancient Remedies', '123 Nile St', 'Cairo', 'Egypt', '78531', '002-123-4567', 'contact@maatremedies.eg'),
+ ('RFWI', 'Royal Family Wind Instruments', '123 Hyrule Castle', 'Hyrule City', 'Hyrule', '77777', '000-555-1234', 'contact@rfwi-hyrule.com'),
+ ('HBA01', 'Hades Battle Axes', '123 Spartan Way', 'Sparta', 'Greece', '23100', '210-123-4567', 'contact@hadesaxes.gr');
+
+
+-- product data
+-- insert some rows into the Product table
+INSERT INTO product (vendorid, partnumber, name, price, unit)
+VALUES (3, 'BW001', 'Bottled Water', 1.50, 'bottle'),
+(4, 'AT101', 'Apple Tree', 29.99, 'tree'),
+(1, 'BS100', 'Bird Seed', 5.99, 'bag'),
+(6, 'GDS001', 'Gold Dragon Statue', 120.00, 'piece'),
+(7, 'UR238', 'Uranium', 75.50, 'gram'),
+(8, 'SR001', 'Stegt rødspætte', 15.99, 'portion'),
+(12, 'OC1234', 'Ocarina', 49.99, 'pcs'),
+(5, 'CPN001', 'Cursed Prosthetic Nose', 19.99, 'pcs'),
+(9, 'BESK001', 'Beskar Chain Mail', 500.00, 'set'),
+(10, 'RING1', 'One Ring', 999.99, 'each'),
+(11, 'MAR101', 'Ostrich Feather', 15.99, 'each'),
+(2, 'OBB234', 'Enzymatic IPA', 20.00, 'pint'),
+(13, 'SGA345', 'Double-sided Axe', 500.99, 'pcs');
+
+
+
+-- Request data
+INSERT INTO request (UserID, Description, Justification, DateNeeded, DeliveryMode)
+VALUES (4, 'Order for more gold', 'Increase treasure hoard', '2941-08-08', 'Rope and Pulley'),
+(6, 'Order for Dinner', 'Will attack if Hungry', '1200-04-30', 'Underwater Delivery'),
+(7, 'Mission to save princess', 'Trying to Save Zelda', '1410-03-25', 'Pickup'),
+(8, 'New nose for Dark Lord', 'Facial reconstruction', '1991-05-01', 'Magic Courier'),
+(1, 'Water for Serpent', 'To sustain environment', '1788-05-10', 'Mystical Rainfall'),
+(10, 'Order of 19 rings', 'To establish societal order', '2010-02-01', 'Discreet Distribution'),
+(10, 'Order of 1 ring', 'To Rule Them All', '2010-02-01', 'Discreet Pickup'),
+(2, 'Replacement trees', 'Replenishing Forest' , '0900-06-06', 'Nature-aligned delivery'),
+(3, 'Replenishing Birdseed' , 'Caring for woodland creatures', '1000-07-02', 'Enchanted Eagle'),
+(5, 'Uranium for Godzilla', 'Will Attack if Hungry', '1956-04-20', 'Subterranean Convoy'),
+(9, 'Custom chainmail', 'Gift for a friend', '1100-01-03', 'Galactic Express'),
+(11, 'Ostrich feather for Anubis', 'To weigh souls fairly', '1650-06-20', 'Mail'),
+(12, 'Beer for Sasquatch', 'For leisurely people-watching', '2024-06-15', 'Stealthy Woodland Drop'),
+(13, 'Double-sided axe for Kratos', 'For enhanced combat', '2005-11-23', 'Pickup');
+
+
+INSERT INTO lineitem (RequestId, ProductId, Quantity)
+VALUES (1, 4, 1),
+(2, 6, 3),
+(3, 7, 1),
+(4, 8, 1),
+(5, 1, 20),
+(6, 10, 19),
+(7, 10, 1),
+(8, 2, 3),
+(9, 3, 3),
+(10, 5, 1),
+(11, 9, 1),
+(12, 11, 1),
+(13, 12, 1), 
+(14, 13, 1);
